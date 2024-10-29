79608475f40b44d5adc09a9e8fbe662a195521d4
benben-xu <benbenben2022@163.com>
2024-10-29 22:45:03 +0800

    Changes

---
+++ day03/src/com/atguigu/java/Person.java
@@ -0,0 +1,15 @@
+package com.atguigu.java;
+
+/**
+ * @author JiaMing_Xu
+ * @create 2024-10-29-22:10
+ */
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
