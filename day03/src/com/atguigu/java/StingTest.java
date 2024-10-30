65ee61ffaaa8696e62007b2475a9ace8e9379a6c
benben-xu <benbenben2022@163.com>
2024-10-30 14:41:23 +0800

    Changes

--- day03/src/com/atguigu/java/StingTest.java
+++ day03/src/com/atguigu/java/StingTest.java
@@ -55,21 +55,44 @@ public class StingTest {
          */
 
         //通过字面量定义的方式：此时的s1和s2的数据javaEE声明在方法区中的字符串常量池中。
-        String s1 = "javaEE";
-        String s2 = "javaEE";
-        //通过new + 构造器的方式：此时的s3和s4保存的地址值，是数据在堆空间开辟空间以后对应的地址值。
-        String s3 = new String("javaEE");
-        String s4 = new String("javaEE");
-        System.out.println(s1 == s2);//true
-        System.out.println(s1 == s3);//false
-        System.out.println(s1 == s4);//false
-        System.out.println(s3 == s4);//false
-        System.out.println("*********");
+//        String s1 = "javaEE";
+//        String s2 = "javaEE";
+//        //通过new + 构造器的方式：此时的s3和s4保存的地址值，是数据在堆空间开辟空间以后对应的地址值。
+//        String s3 = new String("javaEE");
+//        String s4 = new String("javaEE");
+//        System.out.println(s1 == s2);//true
+//        System.out.println(s1 == s3);//false
+//        System.out.println(s1 == s4);//false
+//        System.out.println(s3 == s4);//false
+//        System.out.println("*********");
+//
+//        Person p1 = new Person("Tom",12);
+//        Person p2 = new Person("Tom",12);
+//        System.out.println(p1.name.equals(p2.name));//true
+//        System.out.println(p1.name==p2.name);//true
+//        System.out.println("*********");
 
-        Person p1 = new Person("Tom",12);
-        Person p2 = new Person("Tom",12);
-        System.out.println(p1.name.equals(p2.name));//true
-        System.out.println(p1.name==p2.name);//true
+        /*
+        结论：
+        1.常量与常量的拼接结果在常量池。且常量池中不会存在相同内容的常量。
+        2.只要其中有一个是变量，结果就在堆中。
+        3.如果拼接的结果调用intern()方法，返回值就在常量池中。
+         */
+        String s1 = "javaEE";
+        String s2 = "hadoop";
+        String s3 = "javaEEhadoop";
+        String s4 = "javaEE" + "hadoop";
+        String s5 = s1 + "hadoop";
+        String s6 = "javaEE" + s2;
+        String s7 = s1 + s2;
+        System.out.println(s3 == s4);//true
+        System.out.println(s3 == s5);//false
+        System.out.println(s3 == s6);//false
+        System.out.println(s5 == s6);//false
+        System.out.println(s3 == s7);//false
+
+        String s8 = s5.intern();//返回值得到的s8使用的常量值中已经存在的"javaEEhadoop"
+        System.out.println(s3 == s8);//true
 
 
     }
