c8498c51d528b0e7f52414ad9604e77c5d50fcb7
benben-xu <benbenben2022@163.com>
2024-11-28 22:20:06 +0800

    Changes

--- day04/src/com/atguigu/java/CompareTest.java
+++ day04/src/com/atguigu/java/CompareTest.java
@@ -17,13 +17,15 @@ import java.util.Arrays;
  */
 public class CompareTest {
     /*
-    Comparable接口的使用举例：
+    Comparable接口的使用举例：  自然排序
     1. 像String、包装类等实现了Comparable接口，重写了compareTo()方法，给出了比较两个对象大小的方式。
-    像String、包装类重写CompareTo()方法以后，进行了从小到大的排列
-    2. 重写compareTo(obj)的规则：
+    2. 像String、包装类重写CompareTo()方法以后，进行了从小到大的排列
+    3. 重写compareTo(obj)的规则：
     如果当前对象this大于形参对象obj，则返回正整数，
     如果当前对象this小于形参对象obj，则返回负整数，
     如果当前对象this等于形参对象obj，则返回零。
+    4. 对于自定义类来说，如果需要排序，我们可以让自定义类实现Comparable接口
+       在compareTo(obj)中指定如何排序
 
      */
     @Test
