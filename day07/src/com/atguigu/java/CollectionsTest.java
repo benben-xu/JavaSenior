0aeccca25acf4a1a2764a27b7256adf816a764bb
benben-xu <benbenben2022@163.com>
2024-12-24 13:00:53 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ day07/src/com/atguigu/java/CollectionsTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.java;
+
+/**
+ * Collections:操作Collection、Map的工具类
+ *
+ * 面试题：Collection和Collections的区别？
+ * @author JiaMing_Xu
+ * @create 2024-12-23-20:41
+ */
+public class CollectionsTest {
+    
+}
---
+++ day07/src/com/atguigu/java/PropertiseTest.java
@@ -0,0 +1,19 @@
+package com.atguigu.java;
+
+import java.util.Properties;
+
+/**
+ * @author JiaMing_Xu
+ * @create 2024-12-23-20:05
+ */
+public class PropertiseTest {
+
+    //Propertises:常用来处理配置文件。key和value都是String类型
+    public static void main(String[] args) {
+        Properties prop = new Properties();
+//        FileInputStream fis = new FileInputStream("xxx");
+//        prop.load(fis);//加载流对应的文件
+
+    }
+
+}
